24ac452fd8f3706391d6a9b1bbdaacea5767690b
silvigarvelas <silvigarvelas@hotmail.com>
2021-07-16 23:59:08 +0200

    Para ver si funciona

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,11 +3,13 @@ package Test;
 public class Test {
 	private int numero;
 	private int edadtmp;
+	private String nombre;
 	
-	public Test(int numero, int edadtmp)
+	public Test(int numero, int edadtmp, String nombre)
 	{
 		this.numero=numero;
 		this.edadtmp=edadtmp;
+		this.nombre=nombre;
 	}
 	
 
