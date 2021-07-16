fcc98d1de80db92ec001ca628130999d6c3f2d87
Paprika <Paprika@Paproka>
2021-07-17 00:05:50 +0200

    a

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -4,12 +4,14 @@ public class Test {
 	private int numero;
 	private int edadtmp;
 	private String nombre;
+	private int boba;
 	
-	public Test(int numero, int edadtmp, String nombre)
+	public Test(int numero, int edadtmp, String nombre, int boba)
 	{
 		this.numero=numero;
 		this.edadtmp=edadtmp;
 		this.nombre=nombre;
+		this.boba=boba;
 	}
 	
 
